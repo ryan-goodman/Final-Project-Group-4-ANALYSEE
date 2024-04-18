513ed72eeb6ccd1928c48292423ba8c1ae3d954e
Ryan Goodman <147626268+ryan-goodman@users.noreply.github.com>
2024-04-18 13:15:59 -0400

    Added modularity
    Made prepareState() and drawState() functions

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -26,16 +26,13 @@ void ofApp::setup()
 	{
 		nums[i] = i * 0.5625;
 	}
-	//ofSetFrameRate(10);
-	ofSetColor(255);
 	name.load("fonts/Roboto_Slab/static/RobotoSlab-SemiBold.ttf", 72);
 	nameRect = name.getStringBoundingBox("ANALYSEE", 0, 0);
 	prompt.load("fonts/Kanit/Kanit-Light.ttf", 40);
 	promptRect = prompt.getStringBoundingBox("PRESS ANY KEY TO START", 0, 0);
+	ofSetColor(255);
 	ofNoFill();
 	ofEnableSmoothing();
-	stack.push(0);
-	stack.push(1919);
 }
 
 //--------------------------------------------------------------
@@ -49,117 +46,43 @@ void ofApp::draw()
 {
 	if (state == 0)
 	{
-		name.drawString("ANALYSEE", ofGetWidth() / 2.0 - nameRect.width / 2, 400);
-		if (promptCounter < 120)
-		{
-			prompt.drawString("PRESS ANY KEY TO START", ofGetWidth() / 2.0 - promptRect.width / 2, 800);
-		}
-		promptCounter++;
-		if (promptCounter > 240)
-		{
-			promptCounter = 0;
-		}
-		for (int i = 0; i < 250; i++)
-		{
-			stars[i].displayStar();
-		}
+		drawState0();
 	}
 	else if (state == 1)
 	{
-		// still fading
-		if (fade < 255)
-		{
-			ofBackground(fade);
-			fade++;
-			name.drawString("ANALYSEE", ofGetWidth() / 2.0 - nameRect.width / 2, 400);
-			if (promptCounter < 120)
-			{
-				prompt.drawString("PRESS ANY KEY TO START", ofGetWidth() / 2.0 - promptRect.width / 2, 800);
-			}
-			promptCounter++;
-			if (promptCounter > 240)
-			{
-				promptCounter = 0;
-			}
-			for (int i = 0; i < 250; i++)
-			{
-				stars[i].displayStar();
-			}
-		}
-		// done fading
-		else
-		{
-			ofBackground(40);
-			state = 2;
-			prompt.load("fonts/Kanit/Kanit-Light.ttf", 50);
-			promptRect = prompt.getStringBoundingBox("Visualize a sort", 0, 0);
-		}
-
+		drawState1();
 	}
 	else if (state == 2)
 	{
-		name.drawString("ANALYSEE", ofGetWidth() / 2.0 - nameRect.width / 2, 400);
-		// ofDrawRectangle(ofGetWidth() * 0.75 - promptRect.width / 2 - 25, 725 - promptRect.height, promptRect.width + 50, promptRect.height + 50);
-		prompt.drawString("Visualize a sort", ofGetWidth() * 0.75 - promptRect.width / 2, 750);
+		drawState2();
 	}
 	else if (state == 3)
 	{
-		ofSetColor(0);
-		prompt.drawString("Visualize a sort", ofGetWidth() * 0.75 - promptRect.width / 2, 750);
-		ofSetColor(255);
-		name.drawString("ANALYSEE", ofGetWidth() / 2.0 - nameRect.width / 2, 400);
+		drawState3();
 	}
 	else if (state == 4)
 	{
-		ofSetColor(0);
-		prompt.drawString("Visualize a sort", ofGetWidth() * 0.75 - promptRect.width / 2, 750);
-		ofSetColor(255);
-		name.drawString("ANALYSEE", ofGetWidth() / 2.0 - nameRect.width / 2, 400);
+		// the simple code for drawing doesn't change between 3 and 4
+		// the size of the text changes which is handled elsewhere by loading a different font size
+		drawState3();
 	}
 	else if (state == 5)
 	{
-		prompt.drawString("Quick Sort", ofGetWidth() * 0.25 - promptRect.width / 2, 540);
-		otherPrompt.drawString("Merge Sort", ofGetWidth() * 0.75 - otherPromptRect.width / 2, 540);
+		drawState5();
 	}
 	else if (state == 6)
 	{
-		ofSetColor(0);
-		prompt.drawString("Quick Sort", ofGetWidth() * 0.25 - promptRect.width / 2, 540);
-		ofSetColor(255);
-		otherPrompt.drawString("Merge Sort", ofGetWidth() * 0.75 - otherPromptRect.width / 2, 540);
+		drawState6();
 	}
 	else if (state == 7)
 	{
-		ofSetColor(0);
-		prompt.drawString("Quick Sort", ofGetWidth() * 0.25 - promptRect.width / 2, 540);
-		ofSetColor(255);
-		otherPrompt.drawString("Merge Sort", ofGetWidth() * 0.75 - otherPromptRect.width / 2, 540);
+		// the simple code for drawing doesn't change between 6 and 7
+		// the size of the text changes which is handled elsewhere by loading a different font size
+		drawState6();
 	}
 	else if (state == 8)
 	{
-		ofSetColor(255);
-		for (int i = 0; i < 1920; i++)
-		{
-			ofDrawLine(i, 1080, i, 1080 - nums[i]);
-		}
-		if (!stack.empty())
-		{
-			int high = stack.top();
-			stack.pop();
-			int low = stack.top();
-			stack.pop();
-			int index = passOfQuickSort(nums, low, high);
-			if (index - 1 > low)
-			{
-				stack.push(low);
-				stack.push(index - 1);
-			}
-			if (index + 1 < high)
-			{
-				stack.push(index + 1);
-				stack.push(high);
-			}
-		}
+		drawState8();
 	}
 }
 
@@ -176,10 +99,6 @@ void ofApp::keyPressed(int key)
 	{
 		fade = 255;
 	}
-	else if (state == 2)
-	{
-		// do nothing
-	}
 }
 
 //--------------------------------------------------------------
@@ -193,36 +112,32 @@ void ofApp::mouseMoved(int x, int y)
 {
 	if (state == 2)
 	{
-		// mouse is within the "Visualize a sort" box
-		if (x > ofGetWidth() * 0.75 - promptRect.width / 2 - 25 && x < ofGetWidth() * 0.75 + promptRect.width / 2 + 25
-			&& y > 725 - promptRect.height && y < 725 + promptRect.height)
+		// mouse hovering over Visualize a Sort
+		if (mouseWithin(x, y, promptRect.width, promptRect.height, ofGetWidth() * 0.75, 725 - promptRect.height / 4))
 		{
 			state = 3;
 		}
 	}
 	else if (state == 3)
 	{
-		// mouse just left "Visualize a sort" box
-		if (!(x > ofGetWidth() * 0.75 - promptRect.width / 2 - 25 && x < ofGetWidth() * 0.75 + promptRect.width / 2 + 25
-			&& y > 725 - promptRect.height && y < 725 + promptRect.height))
+		// mouse just left Visualize a Sort
+		if (!(mouseWithin(x, y, promptRect.width, promptRect.height, ofGetWidth() * 0.75, 725 - promptRect.height / 4)))
 		{
 			state = 2;
 		}
 	}
 	else if (state == 5)
 	{
-		// mouse is within Quick Sort box
-		if (x > ofGetWidth() * 0.25 - promptRect.width / 2 - 25 && x < ofGetWidth() * 0.25 + promptRect.width / 2 + 25
-			&& y > 540 - promptRect.height && y < 540 + promptRect.height / 2)
+		// mouse is hovering over Quick Sort
+		if (mouseWithin(x, y, promptRect.width, promptRect.height / 2, ofGetWidth() * 0.25, 540 - promptRect.height / 4))
 		{
 			state = 6;
 		}
 	}
 	else if (state == 6)
 	{
-		// mouse just left Quick Sort box
-		if (!(x > ofGetWidth() * 0.25 - promptRect.width / 2 - 25 && x < ofGetWidth() * 0.25 + promptRect.width / 2 + 25
-			&& y > 540 - promptRect.height && y < 540 + promptRect.height / 2))
+		// mouse just left Quick Sort
+		if (!(mouseWithin(x, y, promptRect.width, promptRect.height / 2, ofGetWidth() * 0.25, 540 - promptRect.height / 4)))
 		{
 			state = 5;
 		}
@@ -241,18 +156,14 @@ void ofApp::mousePressed(int x, int y, int button)
 	// mouse pressed/held while hovering over "Visualize a sort"
 	if (state == 3)
 	{
-		// make smaller
-		prompt.loadFont("fonts/Kanit/Kanit-Light.ttf", 40);
-		// positioning of font is based on this rect
-		promptRect = prompt.getStringBoundingBox("Visualize a sort", 0, 0);
 		state = 4;
+		prepareState4();
 	}
 	// mouse pressed/held over Quick Sort
 	else if (state == 6)
 	{
-		prompt.loadFont("fonts/Kanit/Kanit-Light.ttf", 40);
-		promptRect = prompt.getStringBoundingBox("Quick Sort", 0, 0);
 		state = 7;
+		prepareState7();
 	}
 }
 
@@ -262,20 +173,12 @@ void ofApp::mouseReleased(int x, int y, int button)
 	if (state == 4)
 	{
 		state = 5;
-		ofSetColor(255);
-		prompt.load("fonts/Kanit/Kanit-Light.ttf", 50);
-		otherPrompt.load("fonts/Kanit/Kanit-Light.ttf", 50);
-		promptRect = prompt.getStringBoundingBox("Quick Sort", 0, 0);
-		otherPromptRect = otherPrompt.getStringBoundingBox("Merge Sort", 0, 0);
+		prepareState5();
 	}
 	else if (state == 7)
 	{
 		state = 8;
-		ofSetColor(255);
-		std::random_device random;
-		std::mt19937 g(random());
-		std::shuffle(nums, nums + 1920, random);
-		std::cout << nums[1919];
+		prepareState8();
 	}
 }
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -23,7 +23,145 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-		int passOfQuickSort(float arr[], int low, int high)
+
+		Star stars[250];
+		float nums[1920];
+		ofTrueTypeFont name;
+		ofTrueTypeFont prompt;
+		ofTrueTypeFont otherPrompt;
+		ofRectangle nameRect;
+		ofRectangle promptRect;
+		ofRectangle otherPromptRect;
+		std::stack<int> stack;
+		int promptCounter = 0;
+		int state = 0;
+		int fade = 0;
+
+		void drawState0()
+		{
+			name.drawString("ANALYSEE", ofGetWidth() / 2.0 - nameRect.width / 2, 400);
+			if (promptCounter < 120)
+			{
+				prompt.drawString("PRESS ANY KEY TO START", ofGetWidth() / 2.0 - promptRect.width / 2, 800);
+			}
+			promptCounter++;
+			if (promptCounter > 240)
+			{
+				promptCounter = 0;
+			}
+			for (int i = 0; i < 250; i++)
+			{
+				stars[i].displayStar();
+			}
+		}
+
+		void drawState1()
+		{
+			// still fading
+			if (fade < 255)
+			{
+				ofBackground(fade);
+				fade++;
+				name.drawString("ANALYSEE", ofGetWidth() / 2.0 - nameRect.width / 2, 400);
+				if (promptCounter < 120)
+				{
+					prompt.drawString("PRESS ANY KEY TO START", ofGetWidth() / 2.0 - promptRect.width / 2, 800);
+				}
+				promptCounter++;
+				if (promptCounter > 240)
+				{
+					promptCounter = 0;
+				}
+				for (int i = 0; i < 250; i++)
+				{
+					stars[i].displayStar();
+				}
+			}
+			// done fading
+			else
+			{
+				state = 2;
+				prepareState2();
+			}
+		}
+
+		void drawState2()
+		{
+			name.drawString("ANALYSEE", ofGetWidth() / 2.0 - nameRect.width / 2, 400);
+			prompt.drawString("Visualize a sort", ofGetWidth() * 0.75 - promptRect.width / 2, 750);
+		}
+
+		void drawState3()
+		{
+			ofSetColor(0);
+			prompt.drawString("Visualize a sort", ofGetWidth() * 0.75 - promptRect.width / 2, 750);
+			ofSetColor(255);
+			name.drawString("ANALYSEE", ofGetWidth() / 2.0 - nameRect.width / 2, 400);
+		}
+
+		void drawState5()
+		{
+			prompt.drawString("Quick Sort", ofGetWidth() * 0.25 - promptRect.width / 2, 540);
+			otherPrompt.drawString("Merge Sort", ofGetWidth() * 0.75 - otherPromptRect.width / 2, 540);
+		}
+
+		void drawState6()
+		{
+			ofSetColor(0);
+			prompt.drawString("Quick Sort", ofGetWidth() * 0.25 - promptRect.width / 2, 540);
+			ofSetColor(255);
+			otherPrompt.drawString("Merge Sort", ofGetWidth() * 0.75 - otherPromptRect.width / 2, 540);
+		}
+
+		void drawState8()
+		{
+			ofSetColor(255);
+			for (int i = 0; i < 1920; i++)
+			{
+				ofDrawLine(i, 1080, i, 1080 - nums[i]);
+			}
+			passOfQuickSort();
+		}
+
+		void prepareState2()
+		{
+			ofBackground(40);
+			prompt.load("fonts/Kanit/Kanit-Light.ttf", 50);
+			promptRect = prompt.getStringBoundingBox("Visualize a sort", 0, 0);
+		}
+
+		void prepareState4()
+		{
+			prompt.loadFont("fonts/Kanit/Kanit-Light.ttf", 40);
+			promptRect = prompt.getStringBoundingBox("Visualize a sort", 0, 0);
+		}
+
+		void prepareState5()
+		{
+			ofSetColor(255);
+			prompt.load("fonts/Kanit/Kanit-Light.ttf", 50);
+			otherPrompt.load("fonts/Kanit/Kanit-Light.ttf", 50);
+			promptRect = prompt.getStringBoundingBox("Quick Sort", 0, 0);
+			otherPromptRect = otherPrompt.getStringBoundingBox("Merge Sort", 0, 0);
+		}
+
+		void prepareState7()
+		{
+			prompt.loadFont("fonts/Kanit/Kanit-Light.ttf", 40);
+			promptRect = prompt.getStringBoundingBox("Quick Sort", 0, 0);
+		}
+
+		void prepareState8()
+		{
+			ofSetColor(255);
+			std::random_device random;
+			std::mt19937 g(random());
+			std::shuffle(nums, nums + 1920, random);
+			stack.push(0);
+			stack.push(1919);
+		}
+
+		int partition(float arr[], int low, int high)
 		{
 			float pivot = arr[high];
 			int i = low - 1;
@@ -45,16 +183,36 @@ class ofApp : public ofBaseApp{
 			return i + 1;
 		}
 
-		Star stars[250];
-		float nums[1920];
-		ofTrueTypeFont name;
-		ofTrueTypeFont prompt;
-		ofTrueTypeFont otherPrompt;
-		ofRectangle nameRect;
-		ofRectangle promptRect;
-		ofRectangle otherPromptRect;
-		std::stack<int> stack;
-		int promptCounter = 0;
-		int state = 0;
-		int fade = 0;
+		void passOfQuickSort()
+		{
+			if (!stack.empty())
+			{
+				int high = stack.top();
+				stack.pop();
+				int low = stack.top();
+				stack.pop();
+				int index = partition(nums, low, high);
+				if (index - 1 > low)
+				{
+					stack.push(low);
+					stack.push(index - 1);
+				}
+				if (index + 1 < high)
+				{
+					stack.push(index + 1);
+					stack.push(high);
+				}
+			}
+			else
+			{
+				state = 2;
+				prepareState2();
+			}
+		}
+
+		bool mouseWithin(int x, int y, float rectWidth, float rectHeight, float rectX, float rectY)
+		{
+			return (x > rectX - rectWidth / 2 - 25 && x < rectX + rectWidth / 2 + 25
+					&& y > rectY - rectHeight && y < rectY + rectHeight);
+		}
 };
